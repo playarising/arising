2c2240979c705aa1eabe1e35cc65bed24d9a62b1
Enrique <eabz@kindynos.mx>
2022-09-18 17:52:37 -0500

    Fix flat

--- scripts/flat.sh
+++ scripts/flat.sh
@@ -6,14 +6,9 @@ ROOT=contracts/
 FLAT=flat/
 
 mkdir -p "$FLAT"/base/;
-mkdir -p "$FLAT"/civilizations/;
 mkdir -p "$FLAT"/codex/;
 mkdir -p "$FLAT"/core/;
-mkdir -p "$FLAT"/gadgets/;
 mkdir -p "$FLAT"/items/;
-mkdir -p "$FLAT"/materials/;
-mkdir -p "$FLAT"/materials/raw;
-mkdir -p "$FLAT"/materials/basic;
 
 iterate_sources() {
   files=$(ls "$1"*.sol)
@@ -23,14 +18,10 @@ iterate_sources() {
   done
 }
 
-iterate_sources "$ROOT"/base/ "$FLAT"/base/
-iterate_sources "$ROOT"/civilizations/ "$FLAT"/civilizations/
-iterate_sources "$ROOT"/codex/ "$FLAT"/codex/
-iterate_sources "$ROOT"/core/ "$FLAT"/core/
-iterate_sources "$ROOT"/gadgets/ "$FLAT"/gadgets/
-iterate_sources "$ROOT"/items/ "$FLAT"/items/
-iterate_sources "$ROOT"/materials/basic/ "$FLAT"/materials/basic/
-iterate_sources "$ROOT"/materials/raw/ "$FLAT"/materials/raw/
+iterate_sources "$ROOT"base/ "$FLAT"base/
+iterate_sources "$ROOT"codex/ "$FLAT"codex/
+iterate_sources "$ROOT"core/ "$FLAT"core/
+iterate_sources "$ROOT"items/ "$FLAT"items/
 
 sed -i '' '/SPDX-License-Identifier: MIT/d' flat/**/*.sol
 sed -i '' '/pragma solidity/d' flat/**/*.sol
